4db8bc1567ca8440a581e5161b3878827ccadbb3
alamin24nava <alamin.nava@gmail.com>
2022-06-27 23:27:44 +0600

    gpa calculator function working...

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,61 +11,62 @@ let inputSocialScience = document.getElementById('sScience');
 // Submit Button
 let submitBtn = document.getElementById('btn_submit');
 
-let marksFunction = marks => {
+let gpaFunction = marks => {
   let gpa;
-  if (marks >= 0 && marks <= 32) {
+  if (marks > 0 && marks < 33) {
     gpa = 0;
   } else if (marks >= 33 && marks < 40) {
     gpa = 1;
-  } else if (marks >= 41 && marks < 50) {
+  } else if (marks >= 40 && marks < 50) {
     gpa = 2;
-  } else if (marks >= 51 && marks < 60) {
+  } else if (marks >= 50 && marks < 60) {
     gpa = 3;
-  } else if (marks >= 61 && marks < 70) {
+  } else if (marks >= 60 && marks < 70) {
     gpa = 3.5;
-  } else if (marks >= 71 && marks < 80) {
+  } else if (marks >= 70 && marks < 80) {
     gpa = 4;
-  } else if (marks >= 81 && marks <= 100) {
+  } else if (marks >= 80 && marks <= 100) {
     gpa = 5;
   }
   return gpa;
 };
-let greatPoint = marks => {
+let greatFunction = marks => {
   let great;
-  if (marks >= 0 && marks <= 32) {
+  if (marks > 0 && marks < 33) {
     great = 'F';
   } else if (marks >= 33 && marks < 40) {
     great = 'D';
-  } else if (marks >= 41 && marks < 50) {
+  } else if (marks >= 40 && marks < 50) {
     great = 'C';
-  } else if (marks >= 51 && marks < 60) {
+  } else if (marks >= 50 && marks < 60) {
     great = 'B';
-  } else if (marks >= 61 && marks < 70) {
+  } else if (marks >= 60 && marks < 70) {
     great = 'A-';
-  } else if (marks >= 71 && marks < 80) {
+  } else if (marks >= 70 && marks < 80) {
     great = 'A';
-  } else if (marks >= 81 && marks <= 100) {
+  } else if (marks >= 80 && marks <= 100) {
     great = 'A+';
   }
   return great;
 };
-let cgpa = (ban, eng, math, s, rel, ss) => {
-  let totalGpa =
-    marksFunction(inputBangla.value) +
-    marksFunction(inputEnglish.value) +
-    marksFunction(inputMath.value) +
-    marksFunction(inputScience.value) +
-    marksFunction(inputReligion.value) +
-    marksFunction(inputSocialScience.value);
 
+let cgpaFunction = (ban, eng, math, s, rel, ss) => {
+  let totalGpa =
+    gpaFunction(inputBangla.value) +
+    gpaFunction(inputEnglish.value) +
+    gpaFunction(inputMath.value) +
+    gpaFunction(inputScience.value) +
+    gpaFunction(inputReligion.value) +
+    gpaFunction(inputSocialScience.value);
   totalGpa = (totalGpa / 6).toFixed(2);
+
   if (
-    marksFunction(inputBangla.value) == 0 ||
-    marksFunction(inputEnglish.value) == 0 ||
-    marksFunction(inputMath.value) == 0 ||
-    marksFunction(inputScience.value) == 0 ||
-    marksFunction(inputReligion.value) == 0 ||
-    marksFunction(inputSocialScience.value) == 0
+    gpaFunction(inputBangla.value) == 0 ||
+    gpaFunction(inputEnglish.value) == 0 ||
+    gpaFunction(inputMath.value) == 0 ||
+    gpaFunction(inputScience.value) == 0 ||
+    gpaFunction(inputReligion.value) == 0 ||
+    gpaFunction(inputSocialScience.value) == 0
   ) {
     return `Your Result : F`;
   } else {
@@ -73,67 +74,32 @@ let cgpa = (ban, eng, math, s, rel, ss) => {
   }
 };
 
-submitBtn.addEventListener('click', () => {
-  tab_roll.innerHTML = inputRoll.value;
-  (tab_mark_bangla.innerHTML = inputBangla.value),
-    (tab_mark_english.innerHTML = inputEnglish.value),
-    (tab_mark_math.innerHTML = inputMath.value),
-    (tab_mark_science.innerHTML = inputScience.value),
-    (tab_mark_religion.innerHTML = inputReligion.value),
-    (tab_mark_sscience.innerHTML = inputSocialScience.value);
-  inputBangla.classList.remove('border', 'border-danger');
-  if (inputName.value === '') {
-    inputName.classList.add('border', 'border-danger');
-  } else {
-    tab_name.innerHTML = inputName.value;
-  }
-  if (inputRoll.value === '') {
-    inputRoll.classList.add('border', 'border-danger');
-  } else {
-    tab_roll.innerHTML = inputRoll.value;
-  }
-  if (inputBangla.value === '') {
-    inputBangla.classList.add('border', 'border-danger');
-  } else {
-    tab_gpa_bangla.innerHTML = marksFunction(inputBangla.value);
-    tab_grate_bangla.innerHTML = greatPoint(inputBangla.value);
-    total_cgpa.innerHTML = cgpa();
-  }
-  if (inputEnglish.value === '') {
-    inputEnglish.classList.add('border', 'border-danger');
-  } else {
-    tab_gpa_english.innerHTML = marksFunction(inputEnglish.value);
-    tab_grate_english.innerHTML = greatPoint(inputEnglish.value);
-    total_cgpa.innerHTML = cgpa();
-  }
-  if (inputMath.value === '') {
-    inputMath.classList.add('border', 'border-danger');
-  } else {
-    tab_gpa_math.innerHTML = marksFunction(inputMath.value);
-    tab_great_math.innerHTML = greatPoint(inputMath.value);
-    total_cgpa.innerHTML = cgpa();
-  }
-  if (inputScience.value === '') {
-    inputScience.classList.add('border', 'border-danger');
-  } else {
-    tab_gpa_science.innerHTML = marksFunction(inputScience.value);
-    tab_great_science.innerHTML = greatPoint(inputScience.value);
-    total_cgpa.innerHTML = cgpa();
-  }
-  if (inputReligion.value === '') {
-    inputReligion.classList.add('border', 'border-danger');
-  } else {
-    tab_gpa_religion.innerHTML = marksFunction(inputReligion.value);
-    tab_great_religion.innerHTML = greatPoint(inputReligion.value);
-    total_cgpa.innerHTML = cgpa();
-  }
-  if (inputSocialScience.value === '') {
-    inputSocialScience.classList.add('border', 'border-danger');
-  } else {
-    tab_gpa_sscience.innerHTML = marksFunction(inputSocialScience.value);
-    tab_great_sscience.innerHTML = greatPoint(inputSocialScience.value);
-    total_cgpa.innerHTML = cgpa();
-  }
+cgpaFunction();
 
-  // console.log(marksFunction);
+submitBtn.addEventListener('click', () => {
+  tab_name.innerHTML = inputName.value;
+  tab_roll.innerHTML = inputRoll.value;
+  // all subjects marks
+  tab_mark_bangla.innerHTML = inputBangla.value;
+  tab_mark_english.innerHTML = inputEnglish.value;
+  tab_mark_math.innerHTML = inputMath.value;
+  tab_mark_science.innerHTML = inputScience.value;
+  tab_mark_religion.innerHTML = inputReligion.value;
+  tab_mark_sscience.innerHTML = inputSocialScience.value;
+  // all subject  gpa
+  tab_gpa_bangla.innerHTML = gpaFunction(inputBangla.value);
+  tab_gpa_english.innerHTML = gpaFunction(inputEnglish.value);
+  tab_gpa_math.innerHTML = gpaFunction(inputMath.value);
+  tab_gpa_science.innerHTML = gpaFunction(inputScience.value);
+  tab_gpa_religion.innerHTML = gpaFunction(inputReligion.value);
+  tab_gpa_sscience.innerHTML = gpaFunction(inputSocialScience.value);
+  // all subject great
+  tab_grate_bangla.innerHTML = greatFunction(inputBangla.value);
+  tab_grate_english.innerHTML = greatFunction(inputEnglish.value);
+  tab_great_math.innerHTML = greatFunction(inputMath.value);
+  tab_great_science.innerHTML = greatFunction(inputScience.value);
+  tab_great_religion.innerHTML = greatFunction(inputReligion.value);
+  tab_great_sscience.innerHTML = greatFunction(inputSocialScience.value);
+
+  total_cgpa.innerHTML = cgpaFunction();
 });
